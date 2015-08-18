560fe337cd208521e435f81d0d2643028efb661b
p-v <pratyushverma@outlook.com>
2015-08-18 11:20:30 +0530

    separated circular cell view from base

--- library/src/main/java/com/p_v/flexiblecalendar/view/BaseCellView.java
+++ library/src/main/java/com/p_v/flexiblecalendar/view/BaseCellView.java
@@ -1,10 +1,7 @@
 package com.p_v.flexiblecalendar.view;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.support.annotation.IntDef;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -39,47 +36,25 @@ public class BaseCellView extends TextView {
     public static final int STATE_OUTSIDE_MONTH = R.attr.state_date_outside_month;
 
     private Set<Integer> stateSet;
-    private Paint eventPaint;
-
-    private int eventCircleY;
-    private int radius;
-    private int padding;
-    private int leftMostPosition = Integer.MIN_VALUE;
     private List<Paint> paintList;
 
     public BaseCellView(Context context) {
         super(context);
+        stateSet = new HashSet<>(3);
     }
 
     public BaseCellView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs);
+        stateSet = new HashSet<>(3);
     }
 
     public BaseCellView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs);
-    }
-
-    private void init(AttributeSet attrs){
-        TypedArray a = getContext().obtainStyledAttributes(attrs,R.styleable.BaseCellView);
-        try{
-            radius = (int)a.getDimension(R.styleable.BaseCellView_event_radius,5);
-            padding = (int)a.getDimension(R.styleable.BaseCellView_event_circle_padding,1);
-        }finally {
-            a.recycle();
-        }
         stateSet = new HashSet<>(3);
-        eventPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        eventPaint.setStyle(Paint.Style.FILL);
     }
 
     public void addState(int state){
         stateSet.add(state);
-    }
-
-    public void removeState(int state){
-        stateSet.remove(state);
     }
 
     public void clearAllStates(){
@@ -103,51 +78,6 @@ public class BaseCellView extends TextView {
         }
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        //initialize paint objects only if there is no state or just one state i.e. the regular day state
-        if((stateSet==null || stateSet.isEmpty()
-                ||(stateSet.size() ==1 && stateSet.contains(STATE_REGULAR))) && paintList!=null) {
-            int num = paintList.size();
-
-            Paint p = new Paint();
-            p.setTextSize(getTextSize());
-
-            Rect rect = new Rect();
-            p.getTextBounds("31", 0, 1, rect); // measuring using fake text
-
-            eventCircleY = (3 * getHeight() + rect.height()) / 4;
-
-            //calculate left most position for the circle
-            if (leftMostPosition == Integer.MIN_VALUE) {
-                leftMostPosition = (getWidth() / 2) - (num / 2) * 2 * (padding + radius);
-                if (num % 2 == 0) {
-                    leftMostPosition = leftMostPosition + radius + padding;
-                }
-            }
-
-        }
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        // draw only if there is no state or just one state i.e. the regular day state
-        if((stateSet==null || stateSet.isEmpty() || (stateSet.size() ==1
-                && stateSet.contains(STATE_REGULAR))) && paintList!=null) {
-            int num = paintList.size();
-            for (int i=0;i<num;i++) {
-                canvas.drawCircle(calculateStartPoint(i), eventCircleY, radius, paintList.get(i));
-            }
-        }
-    }
-
-    private int calculateStartPoint(int offset){
-        return leftMostPosition + offset *(2*(radius+padding)) ;
-    }
-
     public void setEvents(List<Integer> colorList){
         if(colorList!=null){
             paintList = new ArrayList<>(colorList.size());
@@ -161,4 +91,13 @@ public class BaseCellView extends TextView {
             requestLayout();
         }
     }
+
+    public Set<Integer> getStateSet(){
+        return stateSet;
+    }
+
+    protected List<Paint> getPaintList(){
+        return paintList;
+    }
+
 }
